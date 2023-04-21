97e2fbf5b4541227df9cdf3fddc1725cb5b3371f
Jake Martin <jakemart@usc.edu>
2023-04-20 18:27:54 -0700

    backend

---
+++ BACKEND studyspots/src/main/java/com/teameight/studyspots/StudyspotsApplication.java
@@ -0,0 +1,13 @@
+package com.teameight.studyspots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyspotsApplication.class, args);
+	}
+
+}
---
+++ BACKEND studyspots/src/test/java/com/teameight/studyspots/StudyspotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teameight.studyspots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
