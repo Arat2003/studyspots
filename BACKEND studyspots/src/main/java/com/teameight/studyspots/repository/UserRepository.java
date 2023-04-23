1b00998bf83615952aef6af22953b8f309213424
Jake Martin <jakemart@usc.edu>
2023-04-23 14:49:16 -0700

    userrepository

--- BACKEND studyspots/src/main/java/com/teameight/studyspots/repository/UserRepository.java
+++ BACKEND studyspots/src/main/java/com/teameight/studyspots/repository/UserRepository.java
@@ -3,8 +3,13 @@ package com.teameight.studyspots.repository;
 import com.teameight.studyspots.entities.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<user, Integer> {
 
     user findByEmail(String email);
+    user validateLogin(String username, String password);
+
+    
 
 }
+
