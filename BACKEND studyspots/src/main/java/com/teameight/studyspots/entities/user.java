0233f9a6c60b07ba5ff5b1fa7d6f58b79b8d4029
Anika Shrivastava <anikashr@usc.edu>
2023-04-23 13:52:06 -0700

    user entity

---
+++ BACKEND studyspots/src/main/java/com/teameight/studyspots/entities/user.java
@@ -0,0 +1,32 @@
+package com.teameight.studyspots.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class user {
+     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+     
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+     
+    @Column(nullable = false, length = 64)
+    private String password;
+     
+    @Column(name = "first_name", nullable = false, length = 20)
+    private String firstName;
+     
+    @Column(name = "last_name", nullable = false, length = 20)
+    private String lastName;
+   
+     
+    // getters and setters are not shown   
+}
---
+++ BACKEND studyspots/src/main/java/com/teameight/studyspots/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.teameight.studyspots.repository;
+
+import com.teameight.studyspots.entities.user;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<user, Integer> {
+
+    user findByEmail(String email);
+
+}
