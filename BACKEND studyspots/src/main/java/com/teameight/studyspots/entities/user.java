fb4e07e554b2d8c885d9bdff45d4d9db5ea3ad78
Jake Martin <jakemart@usc.edu>
2023-04-23 14:48:49 -0700

    edits

--- BACKEND studyspots/src/main/java/com/teameight/studyspots/entities/user.java
+++ BACKEND studyspots/src/main/java/com/teameight/studyspots/entities/user.java
@@ -26,7 +26,48 @@ public class user {
      
     @Column(name = "last_name", nullable = false, length = 20)
     private String lastName;
-   
-     
-    // getters and setters are not shown   
+
+    //no constructor needed..?
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
+
